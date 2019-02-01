523406dca434d2f198080c9d0c36fae2ff865714
Chase Armstrong <chasejarms@gmail.com>
2019-02-01 15:48:10 -0700

    Add in a validator for confirming the password

--- src/Pages/UserSettings/UserSettings.tsx
+++ src/Pages/UserSettings/UserSettings.tsx
@@ -22,6 +22,7 @@ import { UserType } from 'src/Models/userTypes';
 import { IAppState } from 'src/Redux/Reducers/rootReducer';
 import { passwordValidator } from 'src/Validators/password.validator';
 import { requiredValidator } from 'src/Validators/required.validator';
+import firebase from '../../firebase';
 
 export class UserSettingsPresentation extends React.Component<
     IUserSettingsProps,
@@ -43,6 +44,14 @@ export class UserSettingsPresentation extends React.Component<
             ],
         }).markAsInvalid(),
         updatingUserPassword: false,
+    }
+
+    public componentDidMount(): void {
+        const secondPassword = this.state.secondPassword.setValue(this.state.secondPassword.value);
+        secondPassword.validators.push(this.samePasswordForConfirmDialog);
+        this.setState({
+            secondPassword,
+        })
     }
 
     public render() {
@@ -101,12 +110,14 @@ export class UserSettingsPresentation extends React.Component<
                                 value={this.state.firstPassword.value}
                                 onChange={this.handleNewPasswordChange}
                                 type="password"
+                                disabled={this.state.updatingUserPassword}
                             />
                             <TextField
                                 label="Confirm Password"
                                 value={this.state.secondPassword.value}
                                 onChange={this.handleSecondPasswordChange}
                                 type="password"
+                                disabled={this.state.updatingUserPassword}
                             />
                             <div className={resetPasswordButtonContainer}>
                                 <AsyncButton
@@ -126,28 +137,77 @@ export class UserSettingsPresentation extends React.Component<
         )
     }
 
-    private resetUserPassword = (): void => {
-        //
+    private resetUserPassword = async(): Promise<void> => {
+        const user = firebase.auth().currentUser!;
+        try {
+            this.setState({
+                updatingUserPassword: true,
+            })
+            await user.updatePassword(this.state.firstPassword.value)
+        } catch {
+            this.setState({
+                updatingUserPassword: false,
+            })
+            this.resetResetPasswordControls();
+            // tslint:disable-next-line:no-console
+            console.log('updating the password failed');
+        }
+
+        this.setState({
+            updatingUserPassword: false,
+        })
+        this.resetResetPasswordControls();
     }
 
     private handleNewPasswordChange = (event: any): void => {
         const value = event.target.value;
         const firstPasswordFormControlState = this.state.firstPassword.setValue(value);
+        const secondPassword = this.state.secondPassword.setValue(this.state.secondPassword.value);
         this.setState({
             firstPassword: firstPasswordFormControlState,
+            secondPassword,
         })
     }
 
     private handleSecondPasswordChange = (event: any): void => {
         const value = event.target.value;
+        const firstPassword = this.state.firstPassword.setValue(this.state.firstPassword.value);
         const secondPasswordFormControlState = this.state.secondPassword.setValue(value);
         this.setState({
+            firstPassword,
             secondPassword: secondPasswordFormControlState,
         })
     }
 
     private someControlsAreInvalid = (): boolean => {
         return this.state.firstPassword.invalid || this.state.secondPassword.invalid;
+    }
+
+    private resetResetPasswordControls = (): void => {
+        const firstPassword = this.state.firstPassword.setValue('')
+            .markAsInvalid()
+            .markAsTouched()
+            .markAsPristine();
+
+        const secondPassword = this.state.secondPassword.setValue('')
+            .markAsInvalid()
+            .markAsTouched()
+            .markAsPristine();
+
+        this.setState({
+            firstPassword,
+            secondPassword,
+        })
+    }
+
+    private samePasswordForConfirmDialog = (value: any): string | null => {
+        if (this.state.firstPassword.value !== this.state.secondPassword.value) {
+            // tslint:disable-next-line:no-console
+            console.log('the passwords do not match');
+            return 'The passwords must match';
+        } else {
+            return null;
+        }
     }
 }
 
