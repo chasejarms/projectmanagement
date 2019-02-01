de3d29007f3d4423663490456c7acec03aeddc9c
Chase Armstrong <chasejarms@gmail.com>
2019-02-01 14:52:56 -0700

    Add password ui for resetting password

--- src/Pages/UserSettings/UserSettings.ias.tsx
+++ src/Pages/UserSettings/UserSettings.ias.tsx
@@ -1,9 +1,14 @@
 import { css } from 'emotion';
 import { RouteComponentProps } from 'react-router';
+import { FormControlState } from 'src/Classes/formControlState';
 import { IUserSliceOfState } from 'src/Redux/Reducers/userReducer';
 
 // tslint:disable-next-line:no-empty-interface
-export interface IUserSettingsState {}
+export interface IUserSettingsState {
+    firstPassword: FormControlState<string>;
+    secondPassword: FormControlState<string>;
+    updatingUserPassword: boolean;
+}
 
 // tslint:disable-next-line:no-empty-interface
 export interface IUserSettingsProps extends RouteComponentProps<{}> {
@@ -36,10 +41,33 @@ export const createUserSettingsClasses = (
         gridColumnGap: 32,
     });
 
+    const resetPasswordButtonContainer = css({
+        display: 'flex',
+        alignItems: 'flex-end',
+    });
+
+    const resetPasswordContainer = css({
+        paddingTop: 32,
+    });
+
+    const resetPasswordNotTitleContainer = css({
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr 1fr',
+        gridColumnGap: 32,
+    });
+
+    const resetPasswordTitle = css({
+        paddingBottom: 32,
+    });
+
     return {
         userSettingsContainer,
         userSettingsPaper,
         accountSettingsTitle,
         userInformationContainer,
+        resetPasswordButtonContainer,
+        resetPasswordContainer,
+        resetPasswordNotTitleContainer,
+        resetPasswordTitle,
     };
 }
--- src/Pages/UserSettings/UserSettings.tsx
+++ src/Pages/UserSettings/UserSettings.tsx
@@ -16,19 +16,45 @@ import {
 } from '@material-ui/core';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+import { FormControlState } from 'src/Classes/formControlState';
+import { AsyncButton } from 'src/Components/AsyncButton/AsyncButton';
 import { UserType } from 'src/Models/userTypes';
 import { IAppState } from 'src/Redux/Reducers/rootReducer';
+import { passwordValidator } from 'src/Validators/password.validator';
+import { requiredValidator } from 'src/Validators/required.validator';
 
 export class UserSettingsPresentation extends React.Component<
     IUserSettingsProps,
     IUserSettingsState
 > {
+    public state: IUserSettingsState = {
+        firstPassword: new FormControlState({
+            value: '',
+            validators: [
+                requiredValidator(''),
+                passwordValidator,
+            ],
+        }).markAsInvalid(),
+        secondPassword: new FormControlState({
+            value: '',
+            validators: [
+                requiredValidator(''),
+                passwordValidator,
+            ],
+        }).markAsInvalid(),
+        updatingUserPassword: false,
+    }
+
     public render() {
         const {
             userSettingsContainer,
             userSettingsPaper,
             accountSettingsTitle,
             userInformationContainer,
+            resetPasswordContainer,
+            resetPasswordNotTitleContainer,
+            resetPasswordTitle,
+            resetPasswordButtonContainer,
         } = createUserSettingsClasses(this.props, this.state);
 
         const companyId = this.props.location.pathname.split('/')[2];
@@ -67,19 +93,61 @@ export class UserSettingsPresentation extends React.Component<
                             </Select>
                         </FormControl>
                     </div>
-                    {/* <div className={asyncButtonContainer}>
-                        <AsyncButton
-                            color="secondary"
-                            disabled={this.state.caseNotesSaveInProgress || !this.state.notes!.touched || this.state.initialLoadInProgress }
-                            asyncActionInProgress={this.state.caseNotesSaveInProgress}
-                            onClick={this.saveCaseNotes}
-                        >
-                            Save Case Notes
-                        </AsyncButton>
-                    </div> */}
+                    <div className={resetPasswordContainer}>
+                        <Typography variant="title" className={resetPasswordTitle}>Password Reset</Typography>
+                        <div className={resetPasswordNotTitleContainer}>
+                            <TextField
+                                label="New Password"
+                                value={this.state.firstPassword.value}
+                                onChange={this.handleNewPasswordChange}
+                                type="password"
+                            />
+                            <TextField
+                                label="Confirm Password"
+                                value={this.state.secondPassword.value}
+                                onChange={this.handleSecondPasswordChange}
+                                type="password"
+                            />
+                            <div className={resetPasswordButtonContainer}>
+                                <AsyncButton
+                                    asyncActionInProgress={this.state.updatingUserPassword}
+                                    disabled={this.state.updatingUserPassword || this.someControlsAreInvalid()}
+                                    color="secondary"
+                                    variant="contained"
+                                    onClick={this.resetUserPassword}
+                                >
+                                    Reset Password
+                                </AsyncButton>
+                            </div>
+                        </div>
+                    </div>
                 </Paper>
             </div>
         )
+    }
+
+    private resetUserPassword = (): void => {
+        //
+    }
+
+    private handleNewPasswordChange = (event: any): void => {
+        const value = event.target.value;
+        const firstPasswordFormControlState = this.state.firstPassword.setValue(value);
+        this.setState({
+            firstPassword: firstPasswordFormControlState,
+        })
+    }
+
+    private handleSecondPasswordChange = (event: any): void => {
+        const value = event.target.value;
+        const secondPasswordFormControlState = this.state.secondPassword.setValue(value);
+        this.setState({
+            secondPassword: secondPasswordFormControlState,
+        })
+    }
+
+    private someControlsAreInvalid = (): boolean => {
+        return this.state.firstPassword.invalid || this.state.secondPassword.invalid;
     }
 }
 
