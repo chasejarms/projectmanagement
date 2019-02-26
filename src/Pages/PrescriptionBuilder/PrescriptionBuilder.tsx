d71f8ddaaaebb56422aa44be4cdbb87a297d69fa
Chase Armstrong <chasejarms@gmail.com>
2019-02-26 08:43:35 -0700

    Handle the single line input edit component

--- src/Pages/PrescriptionBuilder/PrescriptionBuilder.tsx
+++ src/Pages/PrescriptionBuilder/PrescriptionBuilder.tsx
@@ -777,6 +777,20 @@ export class PrescriptionBuilderPresentation extends React.Component<
                     </div>
                 </div>
             )
+        } else if (control.type === IPrescriptionControlTemplateType.SingleLineText) {
+            return (
+                <div className={threeColumns}>
+                    <div>
+                        <FormControl fullWidth={true}>
+                            <InputLabel>Label</InputLabel>
+                            <Input
+                                value={control.label}
+                                onChange={this.handleControlLabelChange}
+                            />
+                        </FormControl>
+                    </div>
+                </div>
+            )
         }
 
 
