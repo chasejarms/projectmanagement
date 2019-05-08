58d667a47fadb4d4b376403d5e1b68126d053d16
Chase Armstrong <chasejarms@gmail.com>
2019-05-08 10:15:58 -0600

    Stop tracking lib folder

--- functions/lib/models/userTypes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var UserType;
-(function (UserType) {
-    UserType["Doctor"] = "Doctor";
-    UserType["Admin"] = "Admin";
-    UserType["Staff"] = "Staff";
-})(UserType = exports.UserType || (exports.UserType = {}));
-//# sourceMappingURL=userTypes.js.map
