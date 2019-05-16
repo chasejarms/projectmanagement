d63848b7188349a3eb5e8efa53f6f3ccbdf8c639
Chase Armstrong <chasejarms@gmail.com>
2019-05-16 11:01:05 -0600

    Add in first section of the home page

--- src/Pages/Home/Home.ias.tsx
+++ src/Pages/Home/Home.ias.tsx
@@ -1,5 +1,8 @@
+import { WithTheme } from '@material-ui/core';
+import { css } from 'emotion';
+
 // tslint:disable-next-line:no-empty-interface
-export interface IHomeProps {}
+export interface IHomeProps extends WithTheme {}
 // tslint:disable-next-line:no-empty-interface
 export interface IHomeState {}
 
@@ -7,5 +10,54 @@ export const createHomeClasses = (
     props: IHomeProps,
     state: IHomeState,
 ) => {
-    return {};
+    // tslint:disable-next-line:no-console
+    console.log(props);
+
+    const dentalLabCloseUpContainer = css({
+        height: '100vh',
+        backgroundSize: 'cover',
+        display: 'flex',
+        flexDirection: 'column',
+    });
+
+    const topSectionContainer = css({
+        position: 'relative',
+    });
+
+    const navigationBar = css({
+        padding: 16,
+        display: 'flex',
+        justifyContent: 'flex-end',
+        width: '100%',
+        boxSizing: 'border-box',
+        position: 'absolute',
+        top: '0',
+    });
+
+    const dentalLabCloseUpContent = css({
+        flex: 1,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+    });
+
+    const mainTopSectionText = css({
+        color: 'white !important',
+        maxWidth: 800,
+        textAlign: 'center',
+    });
+
+    const seeFeaturesButton = css({
+        marginTop: `36px !important`,
+    });
+
+    return {
+        dentalLabCloseUpContainer,
+        topSectionContainer,
+        navigationBar,
+        dentalLabCloseUpContent,
+        mainTopSectionText,
+        seeFeaturesButton,
+    };
 }
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,10 +1,45 @@
+import {
+    Button,
+    Typography,
+    withTheme,
+} from '@material-ui/core';
 import * as React from 'react';
-import { IHomeProps, IHomeState } from './Home.ias';
+import { createHomeClasses, IHomeProps, IHomeState } from './Home.ias';
 
-export class Home extends React.Component<IHomeProps, IHomeState> {
+// tslint:disable-next-line:no-var-requires
+const dentalLabCloseUpSrc = require('./dental-lab-closeup.png');
+
+export class HomePresentation extends React.Component<IHomeProps, IHomeState> {
     public render() {
+        const {
+            dentalLabCloseUpContainer,
+            topSectionContainer,
+            navigationBar,
+            dentalLabCloseUpContent,
+            mainTopSectionText,
+            seeFeaturesButton,
+        } = createHomeClasses(this.props, this.state);
+
         return (
-            <div>This is the home page component</div>
+            <div>
+                <div className={topSectionContainer}>
+                    <div style={{
+                        backgroundImage: `url(${dentalLabCloseUpSrc})`
+                    }} className={dentalLabCloseUpContainer}>
+                        <div className={navigationBar}>
+                            <Button variant="contained" color="secondary">Login</Button>
+                        </div>
+                        <div className={dentalLabCloseUpContent}>
+                            <Typography variant="h2" className={mainTopSectionText}>
+                                Visual Dental Lab Management
+                            </Typography>
+                            <Button variant="contained" color="secondary" className={seeFeaturesButton}>See Features</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
+
+export const Home = withTheme()(HomePresentation);
